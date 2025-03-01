aa931a1e3f1489a15d8620a55faf9095c94371c3
syd0nya <160541636+syd0nya@users.noreply.github.com>
2025-03-01 19:57:05 +0000

    login changes part idk

--- aura-login/src/index.js
+++ aura-login/src/index.js
@@ -4,12 +4,16 @@ const bcrypt = require("bcrypt");
 
 const app = express();
 
-// use EJS as the view engine
+// Use EJS as the view engine
 app.set('view engine', 'ejs');
 
-// static file
-app.use(express.static("public"));
+// Set the views directory path
+app.set('views', path.join(__dirname, '..', 'views'));
 
+// Serve static files from the public folder
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+// Define routes
 app.get("/login", (req, res) => {
     res.render("login");
 });
@@ -19,10 +23,10 @@ app.get("/signup", (req, res) => {
 });
 
 app.get("/", (req, res) => {
-res.render("login");
+    res.render("login");
 });
 
 const port = 5000;
-app.listen(port, () =>{
+app.listen(port, () => {
     console.log(`Server running on Port: ${port}`);
-})
+});
